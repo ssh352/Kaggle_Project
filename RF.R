d30a49547e0189e963e05cfef9a009a067e2a1c9
Xinyuan Wu <xinyuanwu9@gmail.com>
2016-11-17 20:41:53 -0500

    csv is moved to data

---
+++ RF.R
@@ -0,0 +1,21 @@
+# load the training data
+setwd("C:/Users/Xinyuan Wu/Desktop/Xinyuan's Repo/Kaggle_Project")
+train <- read.csv("data/train.csv/train.csv")
+
+# extract numeric variables
+num <- train[, -c(1: 117, 132)]
+num_scale <- scale(num)
+
+# test colmeans and sd for each column 
+colMeans(num_scale); apply(num_scale, 2, sd)
+
+# perform svd
+num_scale_svd <- svd(num_scale)
+
+# replace numeric variable with svd result
+train[, c(118, 131)] <- num_scale_svd$u
+
+# try rf
+library(randomForest)
+rf1 = randomForest(loss ~ ., data = train, mtry = 50)
+str(train, list.len = ncol(train))
