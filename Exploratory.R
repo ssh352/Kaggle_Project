e52eb859655c4b7170036430f1bebd3f7026836d
Xinyuan Wu <xinyuanwu9@gmail.com>
2016-11-21 23:37:14 -0500

    processed data has been added

--- Exploratory.R
+++ Exploratory.R
@@ -4,54 +4,54 @@ train <- read.csv("data/train.csv/train.csv")
 test <- read.csv("data/test.csv/test.csv")
 num_train <- train[, -c(1: 117, 132)]
 num_test <- test[, -c(1:117)]
-str(num_train); str(num_test)
+# str(num_train); str(num_test)
 
 ## correlation plot
-suppressPackageStartupMessages(library(corrplot))
-correlations <- cor(num_train)
-corrplot.mixed(correlations, upper = "square", order = "hclust")
+# suppressPackageStartupMessages(library(corrplot))
+# correlations <- cor(num_train)
+# corrplot.mixed(correlations, upper = "square", order = "hclust")
 
 ## explore continuous variables
-draw_cont <- function(data, col) {
-    len <- sapply(data, function(x) length(unique(x)))
-    plot(1:len[col], sort(unique(data[, col]), decreasing = FALSE),
-         xlab = paste0('Cont', col), ylab = "Value")
-}
-
-par(mfrow = c(3, 5))
-for (i in 1:14) {
-    draw_cont(num_train, col = i)
-}
-
-par(mfrow = c(3, 5))
-for (i in 1:7) {
-    draw_cont(num_train, col = i)
-    draw_cont(num_test, col = i)
-}
-
-par(mfrow = c(3, 5))
-for (i in 8:14) {
-    draw_cont(num_train, col = i)
-    draw_cont(num_test, col = i)
-}
-
-## check level consistency between train and test
-compare_level <- function(data1, data2) {
-}
-
-## back-transformation of cont variables
-back_trans <- function(data) {
-    for (i in 1:dim(data)[2]) {
-        a <- factor(data[, i], 
-                    levels = sort(unique(data[, i]), decreasing = FALSE))
-        levels(a) <- 1:length(unique(data[, i]))
-        data[, i] <- as.numeric(as.character(a))
-    }
-    return(data)
-}
-
-num_train_trans <- back_trans(num_train)
-num_test_trans <- back_trans(num_test)
+# draw_cont <- function(data, col) {
+#     len <- sapply(data, function(x) length(unique(x)))
+#     plot(1:len[col], sort(unique(data[, col]), decreasing = FALSE),
+#          xlab = paste0('Cont', col), ylab = "Value")
+# }
+# 
+# par(mfrow = c(3, 5))
+# for (i in 1:14) {
+#     draw_cont(num_train, col = i)
+# }
+# 
+# par(mfrow = c(3, 5))
+# for (i in 1:7) {
+#     draw_cont(num_train, col = i)
+#     draw_cont(num_test, col = i)
+# }
+# 
+# par(mfrow = c(3, 5))
+# for (i in 8:14) {
+#     draw_cont(num_train, col = i)
+#     draw_cont(num_test, col = i)
+# }
+# 
+# ## check level consistency between train and test
+# compare_level <- function(data1, data2) {
+# }
+# 
+# ## back-transformation of cont variables
+# back_trans <- function(data) {
+#     for (i in 1:dim(data)[2]) {
+#         a <- factor(data[, i], 
+#                     levels = sort(unique(data[, i]), decreasing = FALSE))
+#         levels(a) <- 1:length(unique(data[, i]))
+#         data[, i] <- as.numeric(as.character(a))
+#     }
+#     return(data)
+# }
+# 
+# num_train_trans <- back_trans(num_train)
+# num_test_trans <- back_trans(num_test)
 
 
 ## Feature Selection
@@ -96,6 +96,18 @@ dmcat_test_proc <- find_same_col(dmcat_train_proc, as.data.frame(dmcat_test))
 
 train_processed <- data.frame(dmcat_train_proc, train[, 117:131])
 test_processed <- data.frame(dmcat_test_proc, test[, 117:130])
+
+# data subset
+set.seed(0)
+train_index <- sample(1:nrow(train_processed), 8*nrow(train_processed)/10)
+test_index <- -train_index
+train <- train_processed[train_index, ]
+test <- train_processed[test_index, ]
+submit <- test_processed
+
+write.csv(train, file = 'train.csv', row.names = F)
+write.csv(test, file = 'test.csv', row.names = F)
+write.csv(submit, file = 'submit.csv', row.names = F)
 
 
 
