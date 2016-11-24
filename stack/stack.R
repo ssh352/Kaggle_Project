7dadabd29edc6d62e90535ba9895b8389499d39e
Xinyuan Wu <xinyuanwu9@gmail.com>
2016-11-24 13:44:06 -0500

    update stack script

--- stack/stack.R
+++ stack/stack.R
@@ -1,5 +1,7 @@
+setwd("c:/Users/Xinyuan Wu/Desktop/Xinyuan's Repo/kaggle_Project")
 library(caret)
 
+# train gam
 result <- read.csv("stack/test_encode2.csv")
 e1_rf <- read.csv("stack/proc_s_RF_t.csv")
 e1_xb <- read.csv("stack/proc_s_XGB2_t.csv")
@@ -16,14 +18,14 @@ pred <- predict(stack_model, df)
 mean(abs(df$loss - pred))
 
 # predict submit
-submission <- read.csv("Prediction/sample_submission.csv")
 e1_rf_s <- read.csv("Prediction/proc_RF_submit_1263_ntree1000.csv")
 e1_xb_s <- read.csv("Prediction/proc_XGB2.csv")
 
 df_s <- data.frame(e1_rf = e1_rf_s$loss, 
                    e1_xb = e1_xb_s$loss)
+
 pred_s <- predict(stack_model, df_s)
 
+submission <- read.csv("Prediction/sample_submission.csv")
 submission$loss <- as.numeric(pred_s)
-
-write.csv(submission, file = 'submission1123.csv', row.names = F)
+write.csv(submission, file = 'submissionNov23.csv', row.names = F)
